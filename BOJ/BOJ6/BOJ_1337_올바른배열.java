f6b129395b93d715d98bff0217da24b98b922586
이승종 <maeugel_rosh@naver.com>
2023-07-21 23:45:02 +0900

    feat: BOJ 1337 올바른 배열

---
+++ BOJ/BOJ6/BOJ_1337_올바른배열.java
@@ -0,0 +1,29 @@
+package BOJ.BOJ6;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BOJ_1337_올바른배열 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+		Collections.sort(list);
+		int ans = 0;
+		for (int i = 0; i < N; i++) {
+			int count = 0;
+			for (int j = list.get(i); j < list.get(i)+5; j++) {
+				if(list.contains(j)) count++;
+			}
+			ans = Integer.max(count, ans);
+		}
+		System.out.println(5-ans);
+	}
+}
